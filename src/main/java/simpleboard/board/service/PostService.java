62d6cb98c56a859ae9d037956cccf535c4a82a8c
FL <refrain2k@gmail.com>
2023-05-10 15:53:14 +0900

    Day#7 삭제 기능 구현

--- src/main/java/simpleboard/board/service/PostService.java
+++ src/main/java/simpleboard/board/service/PostService.java
@@ -3,16 +3,13 @@ package simpleboard.board.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import simpleboard.board.domain.Post;
-import simpleboard.board.domain.PostFilter;
 import simpleboard.board.repository.PostRepository;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,16 +19,11 @@ public class PostService {
     @Autowired
     PostRepository postRepository;
 
-
     @Transactional
     public void savePost(Post post) {
-        post.setPostDate(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd")));
+        post.setPostDate(LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd")));
         post.setContents(post.getContents().replaceAll(System.lineSeparator(), "<br>"));
         postRepository.save(post);
-    }
-
-    public List<Post> findAllPost() {
-        return postRepository.findAll(Sort.by(Sort.Direction.DESC, "postId"));
     }
 
     public Optional<Post> findById(Long Id) {
